f464a63ea25a1cef8806d619db35e7b640d7301d
RNCAT <jaruwat.works@gmail.com>
2021-12-16 17:41:35 +0700

    :sparkles: add components

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,22 @@
+import { Task } from './Task';
+
+export const TASKS: Task[] = [
+  {
+    id: 1,
+    text: 'Wake up',
+    day: 'May 5th at 2:30pm',
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: 'Eat some food',
+    day: 'May 6th at 1:30pm',
+    reminder: true,
+  },
+  {
+    id: 3,
+    text: 'Go to sleep',
+    day: 'May 7th at 12:30pm',
+    reminder: false,
+  },
+];
